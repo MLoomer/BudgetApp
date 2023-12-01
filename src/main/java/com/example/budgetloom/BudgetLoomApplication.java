4f6f218c3ce1676a162c54bb44281baea242962b
MLoomer <112742863+MLoomer@users.noreply.github.com>
2023-11-30 21:55:12 -0600

    initial commit

---
+++ src/main/java/com/example/budgetloom/BudgetLoomApplication.java
@@ -0,0 +1,13 @@
+package com.example.budgetloom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetLoomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudgetLoomApplication.class, args);
+    }
+
+}
