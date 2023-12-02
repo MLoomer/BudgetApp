a6ef19affb99df8d905b8a64ab7d48197153b2eb
MLoomer <112742863+MLoomer@users.noreply.github.com>
2023-12-01 22:38:28 -0600

    save account to repo function added

--- src/main/java/com/example/budgetloom/controllers/AddAccountController.java
+++ src/main/java/com/example/budgetloom/controllers/AddAccountController.java
@@ -1,15 +1,22 @@
 package com.example.budgetloom.controllers;
 
 import com.example.budgetloom.domain.Account;
+import com.example.budgetloom.repositories.AccountRepository;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class AddAccountController {
+    @Autowired
+    private ApplicationContext context;
     @GetMapping("/addaccount")
     public String addAccountPage(Model theModel) {
         Account account=new Account();
@@ -20,7 +27,8 @@ public class AddAccountController {
 
     @PostMapping("/addaccount")
     public String submitForm(@Valid @ModelAttribute("Account") Account account) {
-        //TODO add to repo
-        return "";
+        AccountRepository repo = context.getBean(AccountRepository.class);
+        repo.save(account);
+        return "home";
     }
 }
